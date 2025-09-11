96489ee26f7f7ada7d0afc9a7ecefaa2cc0e13cc
akhand01072004 <akhandchange@gmail.com>
2025-09-11 15:18:42 +0530

    first Unique char

---
+++ first387.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int firstUniqChar(string s) {
+    unordered_map<char,int> freq;
+    for(char c : s){
+        freq[c]++;
+    }
+    for(int i=0; i<s.length(); i++){
+        if(freq[s[i]] == 1) return i;
+    }
+}
+
+int main(){
+    string l = "loveleetcode";
+    cout<<firstUniqChar(l);
+}
