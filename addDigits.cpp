f1c80569067f3d4839bf450cdec13d390923dade
akhand01072004 <akhandchange@gmail.com>
2025-09-11 14:54:26 +0530

    first commit

---
+++ addDigits.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int addDigits(int num){
+    if(num == 0) return 0;
+    if(num % 9 == 0) return 9;
+    else return 1+(num-1)%9;
+
+}
+
+int main(){
+    int num = 38;
+    cout<<addDigits(num);
+
+}
